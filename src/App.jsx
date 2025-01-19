1527c3a379a5a2d0f67439b62a9a2a85d18f749f
Vijaylaxmi-30 <vl9259709@gmail.com>
2025-01-19 18:39:20 +0530

    Leaderboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
+import Header from './component/Header';
+import Leaderboard from './component/Leaderboard';
+import { ThemeProvider } from "./component/theme-provider";
 
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <div className='text-3xl font-bold underline'>
-        <h1>Clash Frontend</h1>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen bg-gradient-game">
+        <div className="min-h-screen backdrop-blur-sm bg-gradient-to-t from-black/20 via-black/15 to-transparent">
+          <Header />
+          <main className="container mx-auto px-4 py-8">
+            <Leaderboard />
+          </main>
+        </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
-export default App
+
+
